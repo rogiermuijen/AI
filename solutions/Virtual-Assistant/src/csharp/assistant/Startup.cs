019373b67156396406ac3943b1b53abd4ef07870
Ryan Lengel Isgrig <rylengel@microsoft.com>
2019-03-27 14:46:05 -0700

    Updated telemetry logging to log user's text

--- solutions/Virtual-Assistant/src/csharp/assistant/Startup.cs
+++ solutions/Virtual-Assistant/src/csharp/assistant/Startup.cs
@@ -123,7 +123,7 @@ namespace VirtualAssistant
                 // Telemetry Middleware (logs activity messages in Application Insights)
                 var sp = services.BuildServiceProvider();
                 var telemetryClient = sp.GetService<IBotTelemetryClient>();
-                var appInsightsLogger = new TelemetryLoggerMiddleware(telemetryClient);
+                var appInsightsLogger = new TelemetryLoggerMiddleware(telemetryClient, logPersonalInformation: true);
                 options.Middleware.Add(appInsightsLogger);
 
                 // Catches any errors that occur during a conversation turn and logs them to AppInsights.
